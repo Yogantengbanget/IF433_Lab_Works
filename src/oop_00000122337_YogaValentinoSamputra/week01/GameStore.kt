b8e2ebbbbf7d9f9725f5555bb6c14e2b183734d4
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-02-05 17:42:44 +0700

    week01: (tugas 2) implement discount logic)

--- src/oop_00000122337_YogaValentinoSamputra/week01/GameStore.kt
+++ src/oop_00000122337_YogaValentinoSamputra/week01/GameStore.kt
@@ -2,7 +2,20 @@ package oop_00000122337_YogaValentinoSamputra.week01
 
 fun main(){
         val gameString = "GTA V"
-        val price = 200.000
+        val price = 200000
 
+}
+
+fun calculateDiscount (price: Int): Int{
+    var discountPrice: Int
+    if (price > 500000)  {
+        discountPrice = price - (price * 20/100)
+    }else {
+        discountPrice = price - (price * 10/100)
+    }
+    return discountPrice;
+}
+
+fun printRecipt (title: String, finalPrice: Int){
 
 }
