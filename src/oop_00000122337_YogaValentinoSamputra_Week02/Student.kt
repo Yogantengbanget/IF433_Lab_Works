226fbcab1058658b897f209bead959752b73698c
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-02-11 15:46:44 +0700

    week02: implement user input and pre-validation

--- src/oop_00000122337_YogaValentinoSamputra_Week02/Student.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week02/Student.kt
@@ -11,7 +11,5 @@ class Student (val name: String, val nim: String, val major : String){
             print("LOG: Objek Student $name berhasil dialokasikan di Memory")
         }
     }
-    constructor(name: String, nim: String) : this (name, nim, major="Non-Matriculated"){
-        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
-    }
+
 }
