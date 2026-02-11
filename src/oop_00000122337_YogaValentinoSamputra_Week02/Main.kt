7081497076a96f36b7f2795c68b526ac845b0487
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-02-11 15:45:12 +0700

    week02: add warning validation in init block

--- src/oop_00000122337_YogaValentinoSamputra_Week02/Main.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week02/Main.kt
@@ -3,7 +3,7 @@ package oop_00000122337_YogaValentinoSamputra_Week02
 import java.util.Scanner
 
 fun main() {
-    val scanner = Scanner (sourcer = System. in)
+    val scanner = Scanner(System.`in`)
 
     println("--- APLIKASI PMB UMN ---");
 
@@ -15,5 +15,13 @@ fun main() {
 
     scanner.nextLine()
 
-    if(nim.lenght)
+    if(nim.length != 5){
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+    }else {
+        print("Masukan Jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+    }
 }
--- src/oop_00000122337_YogaValentinoSamputra_Week02/Student.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week02/Student.kt
@@ -1,5 +1,17 @@
 package oop_00000122337_YogaValentinoSamputra_Week02
 
-class Studen2t (val name: String, val nim: String, val major : String){
+import java.util.Scanner
 
+class Student (val name: String, val nim: String, val major : String){
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!" );
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        }else {
+            print("LOG: Objek Student $name berhasil dialokasikan di Memory")
+        }
+    }
+    constructor(name: String, nim: String) : this (name, nim, major="Non-Matriculated"){
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
 }
