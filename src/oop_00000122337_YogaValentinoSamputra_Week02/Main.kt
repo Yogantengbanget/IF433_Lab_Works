726cd6e310f18190cc96d7f9ec37efb2f1e221cd
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-02-11 15:25:29 +0700

    week02: create basic Student class structure

---
+++ src/oop_00000122337_YogaValentinoSamputra_Week02/Main.kt
@@ -0,0 +1,19 @@
+package oop_00000122337_YogaValentinoSamputra_Week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner (sourcer = System. in)
+
+    println("--- APLIKASI PMB UMN ---");
+
+    print("Masukan Nama : ")
+    val name =  scanner.nextLine()
+
+    print("Masukkan NIM(Wajib 5 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if(nim.lenght)
+}
--- src/oop_00000122337_YogaValentinoSamputra_Week02/Student.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week02/Student.kt
@@ -1,12 +1,5 @@
 package oop_00000122337_YogaValentinoSamputra_Week02
 
-class Student (val name: String, val nim: String, val major : String){
-    init {
-        if (nim.length != 5) {
-            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!" );
-            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
-        }else {
-           print("LOG: Objek Student $name berhasil dialokasikan di Memory")
-        }
-    }
+class Studen2t (val name: String, val nim: String, val major : String){
+
 }
