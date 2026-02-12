c63c062cf4ab3b8db75d7f156ef46a7c858c18d7
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-02-12 09:53:26 +0700

    week02: (task 1) implement library input system

--- src/oop_00000122337_YogaValentinoSamputra_Week02/Loan.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week02/Loan.kt
@@ -1,6 +1,9 @@
 package oop_00000122337_YogaValentinoSamputra_Week02
 
-class loan(val bookTitle: String, val borrower: String, val loanDuration: Int = 1) {
+import java.util.Scanner
+
+
+class Loan(val bookTitle: String, val borrower: String, val loanDuration: Int = 1) {
 
     fun calculateFine(): Int {
         return if (loanDuration > 3) {
@@ -9,5 +12,31 @@ class loan(val bookTitle: String, val borrower: String, val loanDuration: Int = 
              0
         }
     }
+}
+
+fun main () {
+    val scanner = Scanner(System.`in`)
+
+    print("Masukan Judul Buku: ")
+    val bookTitle = scanner.nextLine()
+
+    print("Masukan Nama Anda: ")
+    val borrower = scanner.nextLine()
+
+    print("Masukan durasi peminjaman: ")
+    var loanDuration = scanner.nextInt()
+
+    if (loanDuration <= 0) {
+        loanDuration = 1;
+    }
+
+    val loan = Loan(bookTitle, borrower, loanDuration)
+
+    println("===Detail Peminjaman===")
+    println("Nama Buku: ${loan.bookTitle}")
+    println("Nama Peminjam: ${loan.borrower}")
+    println("Lama Peminjaman: ${loan.loanDuration} hari")
+    println("Total Denda : ${loan.calculateFine()}")
+
 }
 
