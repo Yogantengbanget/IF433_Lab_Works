d14efa7fde77d57eaeb9014b9ee7dc5185038b24
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-02-18 15:30:19 +0700

    week03: fix setter using backing field

--- src/oop_00000122337_YogaValentinoSamputra_Week03/Main.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week03/Main.kt
@@ -1,2 +1,7 @@
 package oop_00000122337_YogaValentinoSamputra_Week03
 
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
--- src/oop_00000122337_YogaValentinoSamputra_Week03/kodingDiKelas.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week03/kodingDiKelas.kt
@@ -1,2 +1,67 @@
 package oop_00000122337_YogaValentinoSamputra_Week03
 
+open class Bapake {
+    private var nama: String = "Belum tau";
+    private var umur:Int = 35;
+    protected var gaji: Int = 1000000;
+
+    public var uangjajan : Int = 0
+        set(value){
+            if(value <= 0 ){
+                println("Masa uang jajan 0 atau minus")
+            }else {
+                field = value;
+            }
+        }
+        get(){
+            return field;
+        }
+
+
+
+    public fun set_nama(namabaru: String){
+        if(this.nama.length <= 0){
+            println("Eh nama gaboleh kosong")
+        }else {
+            this.nama = namabaru;
+        }
+    }
+
+    public fun set_umur(umurbaru: Int){
+        if(this.umur <= 0){
+            println("Eh umur gaboleh kosong")
+        }else {
+                this.umur = umurbaru;
+        }
+    }
+
+    public fun get_nama(): String{
+        return this.nama
+    }
+
+    public fun get_umur(): Int{
+        return this.umur
+    }
+}
+class Anake : Bapake() {
+    fun dapatin_gaji_bapak(): Int {
+        this.gaji = 5500
+        return this.gaji + 100;
+    }
+}
+
+
+
+fun main() {
+    var ank= Anake();
+    ank.set_nama("Udin")
+    println("Nama papi kamu ${ank.get_nama()}")
+    println("Gaji Bapak : " + ank.dapatin_gaji_bapak())
+
+    ank.uangjajan= -100
+}
+
+
+
+
+
