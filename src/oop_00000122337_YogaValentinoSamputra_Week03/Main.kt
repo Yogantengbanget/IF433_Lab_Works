cfbd2f8efc224a2d66b1afd106ee79598164780f
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-02-18 19:00:23 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_00000122337_YogaValentinoSamputra_Week03/Main.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week03/Main.kt
@@ -1,10 +1,16 @@
 package oop_00000122337_YogaValentinoSamputra_Week03
 
 fun main() {
-    val w = Weapon("Pisau")
+    val w = Weapon("Pisau");
+    val p = Player("Veto");
 
     w.damage = -50
     w.damage = 9999
 
     println("${w.tier}")
+
+    println(p.xp)
+
+    p.addXp(50);
+    p.addXp(60);
 }
