ec0adbd01f542e0fe395d1c59be71a9441c00cfa
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-02-18 15:50:13 +0700

    week03: cleanup code

--- src/oop_00000122337_YogaValentinoSamputra_Week03/Main.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week03/Main.kt
@@ -3,11 +3,11 @@ package oop_00000122337_YogaValentinoSamputra_Week03
 fun main() {
     val e = Employee("Budi")
 
-    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
     e.increasesPerformance()
+
 
     println("Pajak yang harus dibayar: ${e.tax}")
 }
