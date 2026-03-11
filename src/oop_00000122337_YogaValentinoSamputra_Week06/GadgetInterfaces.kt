2e5802d2c9a7e426baf3f4535fb9023df3de3b34
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-03-11 15:14:52 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_00000122337_YogaValentinoSamputra_Week06/GadgetInterfaces.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week06/GadgetInterfaces.kt
@@ -1,4 +1,9 @@
 package oop_00000122337_YogaValentinoSamputra_Week06
 
-class GadgetInterfaces {
+interface Camera {
+    fun turnOn(){ println("Lensa kamera terbuka dan sensor aktif.")}
+}
+
+interface Phone{
+    fun turnOn(){println("Sinyal seluler mencari jaringan.")}
 }
--- src/oop_00000122337_YogaValentinoSamputra_Week06/Smartphone.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week06/Smartphone.kt
@@ -1,4 +1,10 @@
 package oop_00000122337_YogaValentinoSamputra_Week06
 
-class Smartphone {
+class Smartphone : Camera, Phone {
+
+    override fun turnOn() {
+        super<Camera>.turnOn()
+        super<Phone>.turnOn()
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
 }
