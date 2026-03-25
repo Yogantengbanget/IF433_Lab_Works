5aa59dc4b13b3794665b774448823e22dda7b690
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-03-25 15:54:09 +0700

    week07: implement data class for structural equality

--- src/oop_00000122337_YogaValentinoSamputra_Week07/UserModels.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week07/UserModels.kt
@@ -1,4 +1,6 @@
 package oop_00000122337_YogaValentinoSamputra_Week07
 
-class UserModels {
-}
+class RegularUser(val name : String, val age: Int)
+
+data class DataUser(val name : String, val age : Int)
+
