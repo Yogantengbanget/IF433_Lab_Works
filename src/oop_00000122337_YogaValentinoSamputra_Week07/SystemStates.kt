cfe69c47bd6ed27a9ace3569c4d99d822bf301f9
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-03-25 15:58:37 +0700

    week07: create ApiResponse sealed class

--- src/oop_00000122337_YogaValentinoSamputra_Week07/SystemStates.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week07/SystemStates.kt
@@ -1,4 +1,11 @@
 package oop_00000122337_YogaValentinoSamputra_Week07
 
-enum class SystemStates {
+enum class AppState {
+    STARTING, RUNNING, STOPPED
+}
+
+sealed class ApiRespone{
+    data class Success(val data : String ) : ApiRespone()
+    data class Error(val messeage : String) : ApiRespone()
+    object Loading : ApiRespone()
 }
