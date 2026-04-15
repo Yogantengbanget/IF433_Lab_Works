17cd0d971f7893d3c66394af86c615d5712abe8a
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-04-15 15:30:59 +0700

    week08: (task) implement payment processing with !!

--- src/oop_00000122337_YogaValentinoSamputra_Week08/JavaPaymentService.java
+++ src/oop_00000122337_YogaValentinoSamputra_Week08/JavaPaymentService.java
@@ -1,4 +1,7 @@
 package oop_00000122337_YogaValentinoSamputra_Week08;
 
 public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
 }
