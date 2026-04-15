91ff65eea14ae6149f34d5afd08c25d9e3a0b51b
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-04-15 15:18:30 +0700

    week08: implement java interop using !!

--- src/oop_00000122337_YogaValentinoSamputra_Week08/LegacyJavaAPI.java
+++ src/oop_00000122337_YogaValentinoSamputra_Week08/LegacyJavaAPI.java
@@ -1,4 +1,7 @@
 package oop_00000122337_YogaValentinoSamputra_Week08;
 
 public class LegacyJavaAPI {
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
 }
