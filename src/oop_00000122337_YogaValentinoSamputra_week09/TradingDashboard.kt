9561a9f3e288ec68657e11fc464f862244e8b406
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-04-22 15:51:42 +0700

    week09: (task) chain sortedBy and map for worst performers

--- src/oop_00000122337_YogaValentinoSamputra_week09/TradingDashboard.kt
+++ src/oop_00000122337_YogaValentinoSamputra_week09/TradingDashboard.kt
@@ -21,6 +21,12 @@ fun main() {
             "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)"
         }
 
+    val worstPerformersString = losingTrades
+        .sortedBy { it.roe }
+        .map {
+            "LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)"
+        }
+
 
 
 }
