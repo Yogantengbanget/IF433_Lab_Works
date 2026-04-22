12d3240da11d8e1c74fb0d95249f169da75f1d20
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-04-22 15:48:36 +0700

    week09: (task) populate mock trade history list

--- src/oop_00000122337_YogaValentinoSamputra_week09/TradingDashboard.kt
+++ src/oop_00000122337_YogaValentinoSamputra_week09/TradingDashboard.kt
@@ -1,2 +1,13 @@
 package oop_00000122337_YogaValentinoSamputra_week09
 
+fun main() {
+
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 15, -10.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 25, 8.3, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 10, 0.0, "OPEN"),
+        TradeLog("BTCUSDT", "LONG", 5, 3.5, "OPEN")
+    )
+}
