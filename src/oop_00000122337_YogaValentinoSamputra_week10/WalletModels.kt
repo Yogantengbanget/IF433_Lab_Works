37dc44c7ba590b96c34afcc1e3d8f4c0fcf89baa
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-05-01 21:47:17 +0700

    week10: (task) create generic ApiResponse wrapper

--- src/oop_00000122337_YogaValentinoSamputra_week10/WalletModels.kt
+++ src/oop_00000122337_YogaValentinoSamputra_week10/WalletModels.kt
@@ -1,2 +1,5 @@
 package oop_00000122337_YogaValentinoSamputra_week10
 
+data class Coin(val name: String, val balance: Double)
+
+data class Transaction(val id: String, val amount : Double)
--- src/oop_00000122337_YogaValentinoSamputra_week10/WalletRepository.kt
+++ src/oop_00000122337_YogaValentinoSamputra_week10/WalletRepository.kt
@@ -1,4 +1,19 @@
 package oop_00000122337_YogaValentinoSamputra_week10
 
-class WalletRepository {
+class WalletRepository<T> {
+
+    private val items = mutableListOf<T>()
+
+    fun add(item: T){
+        items.add(item)
+    }
+
+    fun getAll() : List<T>{
+        return items
+    }
 }
+
+data class ApiResponse<T>(
+    val status: String,
+    val data: T
+)
