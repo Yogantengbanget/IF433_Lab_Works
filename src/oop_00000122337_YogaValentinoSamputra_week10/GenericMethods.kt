0e280a2813b632e54abb7594cb072b92a3d2c301
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-05-01 20:46:33 +0700

    week10: implement generic function with return type

--- src/oop_00000122337_YogaValentinoSamputra_week10/GenericMethods.kt
+++ src/oop_00000122337_YogaValentinoSamputra_week10/GenericMethods.kt
@@ -1,2 +1,9 @@
 package oop_00000122337_YogaValentinoSamputra_week10
 
+fun <T> printData(data:T){
+    println("Data yang diterima: $data")
+}
+
+fun <T> processData(input : T):T {
+    return input
+}
