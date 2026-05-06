e67f20645088fd0818cad244967d2758705e20c7
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-05-06 20:38:35 +0700

    week11: (task) configure AC unit using run

--- src/oop_00000122337_YogaValentinoSamputra_week11/SmartHomeDashboard.kt
+++ src/oop_00000122337_YogaValentinoSamputra_week11/SmartHomeDashboard.kt
@@ -23,4 +23,21 @@ fun main() {
         homeDevices.add(it)
     }
 
+    val acDevice = run {
+        SmartDevice("Daikin Inventer (Kabel Inventer (Kabel 3x2.5)", "HVAC", false, 800)
+    }
+    homeDevices.add(acDevice)
+
+    homeDevices.add(
+        SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10)
+    )
+
+    val searchResult = homeDevices.find {
+        it.category == "Camera"
+    }
+
+    searchResult?.let{
+        println(it.diagnose())
+    }
+
 }
