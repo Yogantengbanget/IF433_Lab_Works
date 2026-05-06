f9336b8164f5e94c7fc21732c7b5a1e40c1b1929
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-05-06 15:34:52 +0700

    week11: implement apply for object initialization

--- src/oop_00000122337_YogaValentinoSamputra_week11/ScopeConfiguration.kt
+++ src/oop_00000122337_YogaValentinoSamputra_week11/ScopeConfiguration.kt
@@ -1,2 +1,12 @@
 package oop_00000122337_YogaValentinoSamputra_week11
 
+data class User(var name: String = "", var age: Int = 0)
+
+fun main (){
+    println("=== TEST APPLY ===")
+    val user = User().apply {
+        name = "Alex"
+        age = 25
+    }
+    println(user)
+}
