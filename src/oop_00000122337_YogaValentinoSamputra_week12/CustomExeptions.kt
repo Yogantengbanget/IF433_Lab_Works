0e113ed204abde4841ce98ffb6c90a14e038cc1b
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-05-13 11:37:46 +0700

    week12: implement throw keyword for validation

--- src/oop_00000122337_YogaValentinoSamputra_week12/CustomExeptions.kt
+++ src/oop_00000122337_YogaValentinoSamputra_week12/CustomExeptions.kt
@@ -1,4 +1,19 @@
 package oop_00000122337_YogaValentinoSamputra_week12
 
-class CustomExeptions {
+class InsufficientFundsException(
+    val amount: Double,
+    val balance: Double
+) : Exception ("Attempted $amount, balance: $balance")
+
+class BankAccount(var balance: Double){
+    fun withdraw(amount : Double){
+        if (amount < 0){
+            throw IllegalArgumentException("Amount must be positive")
+        }
+        if (amount > balance){
+            throw InsufficientFundsException(amount, balance)
+        }
+        balance -= amount
+        println("Withdrawal successful. Remaining balance: $balance")
+    }
 }
