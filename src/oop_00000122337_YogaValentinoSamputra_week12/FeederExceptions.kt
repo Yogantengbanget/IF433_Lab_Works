17f96e3be44ce3f317a2fea778cf5feb2e2b6c48
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-05-13 15:46:43 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000122337_YogaValentinoSamputra_week12/FeederExceptions.kt
+++ src/oop_00000122337_YogaValentinoSamputra_week12/FeederExceptions.kt
@@ -10,4 +10,17 @@ class FoodEmptyException(
 )
 
 class DispenserJamException :
-    FeederException("Wadah dispenser tersangkut/macet!")
+    FeederException("Wadah dispenser tersangkut/macet!"){
+    fun dispenseKibble(
+        requestedGram: Int,
+        availableGram: Int,
+        isJammed: Boolean
+    ): Int {
+
+        require(requestedGram > 0) {
+            "Porsi kibble harus lebih dari 0 gr"
+        }
+
+        return availableGram
+    }
+}
