c50978825ca7145ddfbd0effc76f4ef924aa3b5f
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-05-13 11:25:08 +0700

    week12: implement try as expression

--- src/oop_00000122337_YogaValentinoSamputra_week12/BasicException.kt
+++ src/oop_00000122337_YogaValentinoSamputra_week12/BasicException.kt
@@ -1,2 +1,28 @@
 package oop_00000122337_YogaValentinoSamputra_week12
 
+import java.text.NumberFormat
+
+fun divide(a: Int, b: Int): Int {
+
+    try{
+        return a/b
+    }catch (e: ArithmeticException ){
+        println("Error: ${e.message}")
+        return -1
+    } finally {
+        println("Division attempt finished")
+    }
+}
+
+fun main() {
+    println("=== TEST TRY AS EXPRESSION ===")
+    val inputString = "123A"
+
+    val result: Int = try {
+        Integer.parseInt(inputString)
+    } catch (e: NumberFormatException){
+        -1
+    }
+
+    println("Hasil parsing: $result")
+}
