277980167ea170a5171a1cae2e61eab0abacfe11
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-05-13 15:57:07 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_00000122337_YogaValentinoSamputra_week12/SmartFeeder.kt
+++ src/oop_00000122337_YogaValentinoSamputra_week12/SmartFeeder.kt
@@ -22,3 +22,29 @@ fun dispenseKibble(
 
     return availableGram - requestedGram
 }
+
+fun main() {
+
+    var currentKibbleStock = 50
+
+    try {
+
+        dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+
+    } catch (e: DispenserJamException) {
+
+        println("Error Dispenser: ${e.message}")
+
+    } catch (e: FoodEmptyException) {
+
+        println("Error Stok: ${e.message}")
+
+    } catch (e: Exception) {
+
+        println("General Error: ${e.message}")
+    }
+}
