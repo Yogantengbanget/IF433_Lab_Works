39cfa8a338e786b6b4967f9d63761d307d33a269
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-05-25 14:22:02 +0700

    week14: extract UserValidator and UserRepository

---
+++ src/oop_00000122337_YogaValentinoSamputra_Week14/SrpRefactored.kt
@@ -0,0 +1,20 @@
+package oop_00000122337_YogaValentinoSamputra_Week14
+
+data class User(
+    val name: String,
+    val email: String,
+    val age: Int
+)
+
+class UserValidator {
+
+    fun validate(user: User): Boolean =
+        user.email.contains("@") && user.age >= 18
+}
+
+class UserRepository {
+
+    fun save(user: User) {
+        println("Saving user: ${user.name} to Database")
+    }
+}
