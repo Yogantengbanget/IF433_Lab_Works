08c9fdf22c72290112e037a436d1bee0bae6d2fc
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-05-25 14:22:39 +0700

    week14: extract EmailService to fix SRP

--- src/oop_00000122337_YogaValentinoSamputra_Week14/SrpRefactored.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week14/SrpRefactored.kt
@@ -18,3 +18,11 @@ class UserRepository {
         println("Saving user: ${user.name} to Database")
     }
 }
+
+class EmailService {
+
+    fun sendWelcome(user: User) {
+        println("Welcome email -> ${user.email}")
+    }
+}
+
