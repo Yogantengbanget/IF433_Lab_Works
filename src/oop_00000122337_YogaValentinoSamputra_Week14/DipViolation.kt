057ac334a5892002c5462e01c6b2a43e8561df5c
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-05-25 14:35:26 +0700

    week14: create tight coupled UserService (DIP violation)

---
+++ src/oop_00000122337_YogaValentinoSamputra_Week14/DipViolation.kt
@@ -0,0 +1,15 @@
+package oop_00000122337_YogaValentinoSamputra_Week14
+
+class MySQLDatabase {
+
+    fun query(sql: String): List<String> =
+        listOf("data1", "data2")
+}
+
+class UserService {
+
+    private val database = MySQLDatabase()
+
+    fun getUser(id: Int) =
+        database.query("SELECT * FROM users WHERE id=$id")
+}
