63d01512a73f524ca384d04ba9d3e32a06d1e976
Yogantengbanget <yogavalentinosamputras@gmail.com>
2026-05-25 14:29:09 +0700

    week14: implement Shape independently to fix LSP

--- src/oop_00000122337_YogaValentinoSamputra_Week14/LspRefactored.kt
+++ src/oop_00000122337_YogaValentinoSamputra_Week14/LspRefactored.kt
@@ -3,3 +3,18 @@ package oop_00000122337_YogaValentinoSamputra_Week14
 interface Shape {
     fun area(): Int
 }
+
+class Rectangle(
+    private val width: Int,
+    private val height: Int
+) : Shape {
+
+    override fun area() = width * height
+}
+
+class Square(
+    private val side: Int
+) : Shape {
+
+    override fun area() = side * side
+}
